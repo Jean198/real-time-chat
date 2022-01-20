403d5daf907b915224a294cf6ea7c0c30cc49051
Jean198 <jeandedieu.niyigaba@gmail.com>
2022-01-20 13:38:21 +0200

    third commit

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const server=app.listen(3000, ()=>{
 const io = socket(server)
 
 app.use(express.static(path.join(__dirname, '/public')));
-const PORT= process.env.PORT || 3000;
+const PORT=3000;
 
 
 const chatName="niyigabaChat"
