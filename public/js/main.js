aac8e8a4493f509051a21f33541b3aa35e441073
Jean198 <jeandedieu.niyigaba@gmail.com>
2022-01-20 14:05:05 +0200

    changed tho socket listen url

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@
 const chatForm= document.getElementById('chat-form');
 const chatMessages=document.querySelector('.chat-messages')
 
-const socket=io.connect('https://vercel.com/jean198/real-time-chat')
+const socket=io.connect('https://vercel.com/jean198/')
 
 
 
